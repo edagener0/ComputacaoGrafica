bf5a1fcac4bd6371ced36d848f9e6ff4c51d6ca0
edagener <edagener0@users.noreply.github.com>
2026-02-10 17:34:23 +0000

    first commit

---
+++ projetos/P2/p2-2/core/input.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+class Input(object):
+    """Manages the users commands"""
+    def __init__(self) -> None:
+        """User terminated?"""
+        self.quit = False
+
+    def update(self):
+        """Manage user input events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit = True
+            
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.quit = True
